45d95186670b30fd86143c4499cc3f01c47f6e86
Davo <davidgalavotti@gmail.com>
2014-08-20 22:35:57 -0300

    Agrego line chart historico de 2 sensores simulatenamente

--- frontend/js/historico.js
+++ frontend/js/historico.js
@@ -1,77 +1,149 @@
 var socket, valores, margin, width, height, n, margin;
-var x, y, line, svg, path, rango;
+var xSound, ySound, yAxisSound, lineSound, svgSound, pathSound, rangeSound, dataSound, textSound;
+var xLight, yLight, yAxisLight, lineLight, svgLight, pathLight, rangeLight, dataLight, textLight;
 
 socket = io.connect('//localhost:3000');
 
 
+    valores = [0,0,0,0];
 
-    var MINSound, MAXSound, maxSound;
+    margin = {top: 0, right: 0, bottom: 25, left: 15};
+
+    n = 100, // cantidad de ticks en X
+    dataSound = d3.range(n),
+    dataLight = d3.range(n);
+
+    width = $('#fiebre-sonido').width() - margin.left - margin.right;
+    height = $('#fiebre-sonido').height() - margin.top - margin.bottom;
+
+var MINSound, MAXSound, maxSound;
 
 
-    
-        MINSound = 10;   // valor Minimo del sensor de sonido
-        MAXSound = 140;  // valor Maximo del sensor de sonido
-        maxSound = MAXSound;  // var: Maximo de escala normal de sonido
+
+    MINSound = 0;   // valor Minimo del sensor de sonido
+    MAXSound = 140;  // valor Maximo del sensor de sonido
+    maxSound = MAXSound;  // var: Maximo de escala normal de sonido
+                          
+
+var MINLight, MAXLight, maxLight;
+
+
+    MINLight = 1;   // valor Minimo del sensor de sonido
+    MAXLight = 500;  // valor Maximo del sensor de sonido
+    maxLight = MAXLight;  // var: Maximo de escala normal de sonido
 
 
 
-
-
-valores = [0,0,0,0];
-
-margin = {top: 20, right: 0, bottom: 150, left: 0};
-
-width = $('#fiebre-sonido').width();
-height = $('#fiebre-sonido').height();
-
-n = 100, // cantidad de ticks en X
-data = d3.range(n);
-
-x = d3.scale.linear()
+xLight = d3.scale.linear()
     .domain([ 1 , n - 2 ])
     .range([ 0 , width ]);
 
-y = d3.scale.linear()
-    .domain([ maxSound,MINSound ])
+yLight = d3.scale.linear()
+    .domain([ MINLight, maxLight ])
     .range([ 0,height ]);
 
-line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d, i) { return x(i); })
-    .y(function(d, i) { return y(d); });
+yAxisLight = d3.svg.axis()
+    .scale(yLight)
+    .tickSize(-width)
+    .orient("left")
+    .ticks(5, " db");
 
-svg = d3.select("#fiebre-sonido").append("svg")
-    .attr("width", '100%')
-    .attr("height", height)
-    .attr('viewBox','-40 20 '+(width-margin.left) +' '+(height-margin.bottom) )
-    .attr('preserveAspectRatio','xMidYMid') //none para streched
+lineLight = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d, i) { return xLight(i); })
+    .y(function(d, i) { return yLight(d); });
+
+svgLight = d3.select("#fiebre").append("svg")
+    .attr("width", width-margin.left )
+    .attr("height", height + margin.top + margin.bottom)
+    .attr('viewBox','-45 10 '+(width) +' '+(height) )
+    // .attr('preserveAspectRatio','xMidYMid') //none para streched
     .append("g")
     .attr("transform", "translate(" + Math.min(width,height) /1000 + "," + Math.min(height,width) /1000 + ")");
 
-svg.append("defs").append("clipPath")
+svgLight.append("g")
+    .attr("class", "y axis")
+    .call(yAxisLight);
+
+svgLight.append("defs").append("clipPath")
     .attr("id", "clip")
     .append("rect")
     .attr("width", width)
     .attr("height", height);
 
-path = svg.append("g")
+pathLight = svgLight.append("g")
     .attr("clip-path", "url(#clip)")
     .append("path")
-    .datum(data)
-    .attr("class", "line")
-    .attr("d", line);
+    .datum(dataLight)
+    .attr("class", "line light")
+    .attr("d", lineLight);
     // Cambiar opacidad de 0 a 1 cuando hayan datos.
 
-texto = svg.append("text")
+textLight = svgLight.append("text")
     .text("")
     .attr("text-anchor", "right")
-    .attr("class", "numero")
+    .attr("class", "numberLight")
     .attr("dy", 50)
     .attr("dx", 50);
 
-svg.append("g")
+
+
+
+xSound = d3.scale.linear()
+    .domain([ 1 , n - 2 ])
+    .range([ 0 , width ]);
+
+ySound = d3.scale.linear()
+    .domain([ maxSound,MINSound ])
+    .range([ 0,height ]);
+
+yAxisSound = d3.svg.axis()
+    .scale(ySound)
+    .tickSize(-width)
+    .orient("left")
+    .ticks(5, " db");
+
+lineSound = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d, i) { return xSound(i); })
+    .y(function(d, i) { return ySound(d); });
+
+svgSound = d3.select("#fiebre-sonido").append("svg")
+    .attr("width", width-margin.left )
+    .attr("height", height + margin.top + margin.bottom)
+    .attr('viewBox','-45 10 '+(width) +' '+(height) )
+    // .attr('preserveAspectRatio','xMidYMid') //none para streched
+    .append("g")
+    .attr("transform", "translate(" + Math.min(width,height) /1000 + "," + Math.min(height,width) /1000 + ")");
+
+svgSound.append("g")
     .attr("class", "y axis")
-    .call(d3.svg.axis().scale(y).orient("left").ticks(5, " db"));
+    .call(yAxisSound);
+
+svgSound.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+
+pathSound = svgSound.append("g")
+    .attr("clip-path", "url(#clip)")
+    .append("path")
+    .datum(dataSound)
+    .attr("class", "line sound")
+    .attr("d", lineSound);
+    // Cambiar opacidad de 0 a 1 cuando hayan datos.
+
+textSound = svgSound.append("text")
+    .text("")
+    .attr("text-anchor", "right")
+    .attr("class", "numberSound")
+    .attr("dy", 50)
+    .attr("dx", 50);
+
+
+
+
 
 // listener de mensajes de socket.io
 socket.on('message', function (message) {
@@ -81,43 +153,78 @@ socket.on('message', function (message) {
 setInterval(
     function(){
 
+        if (maxLight < valores[3] ){
+            maxLight = valores[3] ;
+        } else {
+            if (maxLight >= MAXLight) {
+                maxLight--;
+            }
+            // actualizo eje Y con el nuevo máximo
+            yLight = d3.scale.linear()
+                .domain([ maxLight,MINLight ])
+                .range([ 0, height ]);
+
+            //call sin transicion porque va tan rapido que se pierde
+            svgLight.select(".y")
+                .call(yAxisLight);
+        }
+
+        rangeLight = d3.scale.linear().domain([ MINLight, maxLight ]).range([ 0.2 , maxLight  ]);
+        dataLight.push(rangeLight ( valores[3] ) );
+
+        // redibuja y corre la linea
+        pathLight
+            .attr("d", lineLight)
+            .attr("transform", null)
+            .transition()
+            .ease("linear")
+            .attr("transform", "translate(" + xLight(0) + ",0)");
+
+        // elimino el dato mas viejo del array
+        dataLight.shift();
+
+        // actualizo el texto con el valor actual
+        svgLight.select(".numberLight")
+            .text(
+                function () {
+                    return ( valores[3] + " lm");
+            });
+
         if (maxSound < valores[2] ){
             maxSound = valores[2] ;
         } else {
             if (maxSound >= MAXSound) {
                 maxSound--;
-        }
+            }
             // actualizo eje Y con el nuevo máximo
-            y = d3.scale.linear()
+            ySound = d3.scale.linear()
                 .domain([ maxSound,MINSound ])
                 .range([ 0, height ]);
 
-
-
             //call sin transicion porque va tan rapido que se pierde
-            svg.select(".y")
-                .call(d3.svg.axis().scale(y).orient("left").ticks(5, " db"));
-            }
+            svgSound.select(".y")
+                .call(yAxisSound);
+        }
 
-        rango = d3.scale.linear().domain([ MINSound, maxSound ]).range([ 0.2 , maxSound  ]);
-        data.push(rango ( valores[2] ) );
+        rangeSound = d3.scale.linear().domain([ MINSound, maxSound ]).range([ 0.2 , maxSound  ]);
+        dataSound.push(rangeSound ( valores[2] ) );
 
         // redibuja y corre la linea
-        path
-            .attr("d", line)
+        pathSound
+            .attr("d", lineSound)
             .attr("transform", null)
             .transition()
             .ease("linear")
-            .attr("transform", "translate(" + x(0) + ",0)");
+            .attr("transform", "translate(" + xSound(0) + ",0)");
 
         // elimino el dato mas viejo del array
-        data.shift();
+        dataSound.shift();
 
         // actualizo el texto con el valor actual
-        svg.select("text")
+        svgSound.select(".numberSound")
             .text(
                 function () {
-                    return ( valores[2] + " db");
+                    return ( valores[2] + " dB");
             });
         }
 ,280);
