7c984a98cca5d711249ec21bba3d7bceba31ce99
Nicolás Lound <nicolas.lound@gmail.com>
2014-08-21 12:21:02 -0300

    Elimino la temperatura

--- frontend/js/temperatura.js
+++ frontend/js/temperatura.js
@@ -17,55 +17,26 @@ MAXHum = 100;   // valor MAXIMO del sensor
 
 pi = 2 * Math.PI; 
 
-getArcTemp = d3.svg.arc().innerRadius(170).outerRadius(200).startAngle(0);
-arc = d3.svg.arc().innerRadius(170).outerRadius(200).startAngle(0);
-
-// colorScaleTemp = d3.scale.linear().domain([MINTemp, MAXTemp]).range(["#2F004B", "#554B80", "#BFA1B4", "#E6CA94","#FFF288"]);
-
-svgTemp = d3.select("#arcTemp").append("svg")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .attr('viewBox','0 0 '+(widthArcs) +' '+(heightArcs) )
-    .append("g")
-    .attr("transform", "translate(" + widthArcs / 2 + "," + heightArcs / 2 + ")")
-
-// Añado el arco del fondo
-bkgTemp = svgTemp.append("path")
-    .datum({endAngle: pi})
-    .style("fill", "#ffffff")
-    .attr("d", getArcTemp);
-
-// Añado el arco de color
-arcTemp = svgTemp.append("path")
-    .datum({endAngle: 0 * pi})
-    .attr("d", getArcTemp);
-
-textTemp = svgTemp.append("text")
-    .text("0°")
-    .attr("class", "degree")
-    .attr("dy", 45)
-    .attr("dx", 25)
-    .style("text-anchor","middle");
-
-colorScaleHum = d3.scale.linear().domain([MINHum, MAXHum]).range(["#2F004B", "#554B80", "#BFA1B4", "#E6CA94","#FFF288"]);
+arcArcHum = d3.svg.arc().innerRadius(170).outerRadius(200).startAngle(0);
 
 svgHum = d3.select("#arcHum").append("svg")
     .attr("width", "100%")
     .attr("height", "100%")
     .attr('viewBox','0 0 '+(widthArcs) +' '+(heightArcs) )
     .append("g")
-    .attr("transform", "translate(" + widthArcs / 2 + "," + heightArcs / 2 + ")")
+    .attr("transform", "translate(" + widthArcs / 2 + "," + heightArcs / 2 + ")");
 
 // Añado el arco del fondo
 bkgHum = svgHum.append("path")
     .datum({endAngle: pi})
     .style("fill", "#ffffff")
-    .attr("d", arc);
+    .attr("d", arcArcHum);
 
 // Añado el arco de color
 arcHum = svgHum.append("path")
     .datum({endAngle: 0 * pi})
-    .attr("d", arc);
+    .attr("class","pathHum")
+    .attr("d", arcArcHum);
 
 textHum = svgHum.append("text")
     .text("0"+'%')
@@ -77,47 +48,34 @@ textHum = svgHum.append("text")
 // oigo el mensaje del Arduino y cambio el valor del ángulo
 socket.on('message', function (message) {
     valores[message[message.split(",")[0].length - 1]] = parseInt(message.split(",")[1]);
-    // solo proceso los mensajes del /sensor_0
     
 }); // end listener   
 
 setInterval(
     function(){
-
-            svgTemp.select('.degree')
-                .text(function () {
-                    return (valores[0] + "°");
-                });
-
-            rangeTemp = d3.scale.linear().domain([ MINTemp , MAXTemp ]).range([  0 , pi ]);
-
-            arcTemp.transition()
-                .ease("linear")
-                .call(arcTween, rangeTemp ( valores[0] ) );
-
-
             svgHum.select('.percent')
                 .text(function () {
                     return (valores[1] + "%");
                 });
 
+
             rangeHum = d3.scale.linear().domain([ MINHum , MAXHum ]).range([  0 , pi ]);
 
             arcHum.transition()
                 .ease("linear")
-                .call(arcTween, rangeHum ( valores[1] ) );
+                .call(arcTweenHum, rangeHum( valores[1] ) );
 
+    },280);
 
-          // 
-    },280)
 
 // Hace el tween entre el ángulo viejo y el nuevo
-function arcTween(transition, newAngle) {
-  transition.attrTween("d", function(d) {
-    interpolate = d3.interpolate(d.endAngle, newAngle);
+function arcTweenHum(transition, newAngle) {
+  transition.attrTween("d", function(e) {
+    interpolate = d3.interpolate(e.endAngle, newAngle);
     return function(t) {
-      d.endAngle = interpolate(t);
-      return arc(d);
+      e.endAngle = interpolate(t);
+      return arcArcHum(e);
     };
   });
 }
+
