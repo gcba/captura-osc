52fc83f9fe99330db76acb89f9a17d11c5f3103b
Nicolás Lound <nicolas.lound@gmail.com>
2014-08-21 14:43:29 -0300

    Dashboard con Lux, Ruido y Temperatura

--- frontend/js/historico.js
+++ frontend/js/historico.js
@@ -1,6 +1,8 @@
 var socket, valores, margin, width, height, n, margin, bkg, colorScale;
 var xSound, ySound, yAxisSound, lineSound, svgSound, pathSound, rangeSound, dataSound, textSound, MINSound, MAXSound, maxSound;
 var xLight, yLight, yAxisLight, lineLight, svgLight, pathLight, rangeLight, dataLight, textLight, MINLight, MAXLight, maxLight;
+var MINTemp, MAXTemp, svgTemp, getArcTemp, arcTemp, bkgTemp, rangeTemp, colorScaleTemp, textTemp;
+
 
 socket = io.connect('//localhost:3000');
 
@@ -138,6 +140,44 @@ textSound = svgSound.append("text")
 
 
 
+// Temperatura
+widthArcs = $('.arco').width()+150;
+heightArcs = $('.arco').height()+150;
+
+MINTemp = 0;    // valor MINIMO del sensor
+MAXTemp = 40;   // valor MAXIMO del sensor
+
+pi = 2 * Math.PI; 
+
+getArcTemp = d3.svg.arc().innerRadius(170).outerRadius(200).startAngle(0);
+arc = d3.svg.arc().innerRadius(170).outerRadius(200).startAngle(0);
+
+svgTemp = d3.select("#arcTemp").append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr('viewBox','0 0 '+(widthArcs) +' '+(heightArcs) )
+    .append("g")
+    .attr("transform", "translate(" + widthArcs / 2 + "," + heightArcs / 2 + ")")
+
+// Añado el arco del fondo
+bkgTemp = svgTemp.append("path")
+    .datum({endAngle: pi})
+    .style("fill", "#ffffff")
+    .attr("d", getArcTemp);
+
+// Añado el arco de color
+arcTemp = svgTemp.append("path")
+    .datum({endAngle: 0 * pi})
+    .attr("d", getArcTemp);
+
+textTemp = svgTemp.append("text")
+    .text("0°")
+    .attr("class", "degree")
+    .attr("dy", 45)
+    .attr("dx", 25)
+    .style("text-anchor","middle");
+
+
 
 
 // listener de mensajes de socket.io
@@ -147,6 +187,20 @@ socket.on('message', function (message) {
 
 setInterval(
     function(){
+
+
+        svgTemp.select('.degree')
+            .text(function () {
+                return (valores[0] + "°");
+            });
+
+        rangeTemp = d3.scale.linear().domain([ MINTemp , MAXTemp ]).range([  0 , pi ]);
+
+        arcTemp.transition()
+            .ease("linear")
+            .call(arcTween, rangeTemp ( valores[0] ) );
+
+
 
         if (maxLight < valores[3] ){
             maxLight = valores[3] ;
